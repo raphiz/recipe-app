dc48bb0545b301208eeea02587a70c25bc4b0d72
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2023-06-21 07:12:40 +0200

    Replace search submit form with instant search form

--- app/src/main/kotlin/li/raphael/recipe/app/shared/components/SearchForm.kt
+++ app/src/main/kotlin/li/raphael/recipe/app/shared/components/SearchForm.kt
@@ -2,10 +2,15 @@ package li.raphael.recipe.app.shared.components
 
 import kotlinx.html.*
 import li.raphael.kotlinhtml.templatecontext.t
+import li.raphael.kotlinhtml.utils.dataAttributes
 
 fun FlowContent.uSearchForm(fieldName: String, searchText: String?, customizer: FORM.() -> Unit = {}) {
     form(classes = "usa-search margin-bottom-4") {
         customizer()
+        dataAttributes(
+            "action" to "keyup->search-form#debouncedSubmit search->search-form#debouncedSubmit",
+            "controller" to "search-form",
+        )
         role = "search"
         label {
             htmlFor = fieldName
@@ -15,14 +20,6 @@ fun FlowContent.uSearchForm(fieldName: String, searchText: String?, customizer: 
             this.id = fieldName
             searchText?.let { value = searchText }
             placeholder = t("search.field.label")
-        }
-        uButton {
-            type = ButtonType.submit
-            span("usa-search__submit-text") { +t("search.button.label") }
-            img(classes = "usa-search__submit-icon") {
-                src = "/img/usa-icons-bg/search--white.svg"
-                alt = t("search.button.label")
-            }
         }
     }
 }
